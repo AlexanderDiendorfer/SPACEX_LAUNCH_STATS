70e64b6e1bc461894a43900878064c53ac4a8b4f
Alexander Diendorfer+ <20122072@htlneufelden.onmicrosoft.comgit config --global user.email 20122072@htlneufelden.onmicrosoft.comgit config --global user.email 20122072@htlneufelden.onmicrosoft.com>
2018-11-13 21:08:06 +0100

    Initial Commit
    Initial commit of the SPACEX_LAUNCH_STATS App

---
+++ public/precache-manifest.8b0cba783b5746f39b498f2a25c53fad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3dbe9ab8f736a2d9af903f8d34c6683",
+    "url": "/static/media/spacex-logo.c3dbe9ab.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8891c7ab5c24b5556a94",
+    "url": "/static/js/main.8891c7ab.chunk.js"
+  },
+  {
+    "revision": "cf5be8d71f67ac242686",
+    "url": "/static/js/1.cf5be8d7.chunk.js"
+  },
+  {
+    "revision": "8891c7ab5c24b5556a94",
+    "url": "/static/css/main.971f3851.chunk.css"
+  },
+  {
+    "revision": "24cc237cbb266b11ae015bc3a2114d5e",
+    "url": "/index.html"
+  }
+];
